d620285f0a7df0844105e3225aee5b100563d4f1
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2023-04-14 22:41:49 -0500

    add letterIndex to return proper index for letters

---
+++ frontend/src/Pages/RestaurantSelector/letterIndex.js
@@ -0,0 +1,88 @@
+function letterIndex(letter){
+    let letterIndex = ''
+    switch(letter){
+        case 'A':
+            letterIndex = 0
+            break;
+        case 'B':
+            letterIndex = 1
+            break;
+        case 'C':
+            letterIndex = 2
+            break;
+        case 'D':
+            letterIndex = 3
+            break;
+        case 'E':
+            letterIndex = 4
+            break;
+        case 'F':
+            letterIndex = 5
+            break;
+        case 'G':
+            letterIndex = 6
+            break;
+        case 'H':
+            letterIndex = 7
+            break;
+        case 'I':
+            letterIndex = 8
+            break;
+        case 'J':
+            letterIndex = 9
+            break;
+        case 'K':
+            letterIndex = 10
+            break;
+        case 'L':
+            letterIndex = 11
+            break;
+        case 'M':
+            letterIndex = 12
+            break;
+        case 'N':
+            letterIndex = 13
+            break;
+        case 'O':
+            letterIndex = 14
+            break;
+        case 'P':
+            letterIndex = 15
+            break;
+        case 'Q':
+            letterIndex = 16
+            break;
+        case 'R':
+            letterIndex = 17
+            break;
+        case 'S':
+            letterIndex = 18
+            break;
+        case 'T':
+            letterIndex = 19
+            break;
+        case 'U':
+            letterIndex = 20
+            break;
+        case 'V':
+            letterIndex = 21
+            break;
+        case 'W':
+            letterIndex = 22
+            break;
+        case 'X':
+            letterIndex = 23
+            break;
+        case 'Y':
+            letterIndex = 24
+            break;
+        case 'Z':
+            letterIndex = 25
+            break;
+        }
+
+    return(
+        letterIndex
+    )
+}
+export default letterIndex;
