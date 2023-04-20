7739112d9968003dd14e08e2f6c129d56058b427
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2023-04-19 20:49:42 -0500

    update file locations

---
+++ frontend/src/Pages/Users/FindUsers/findUsers.js
@@ -0,0 +1,15 @@
+import Container from "react-bootstrap/esm/Container";
+function findUsers(){
+    return(
+        <div>
+            <Container>
+                <h1>Find users</h1>
+            </Container>
+            
+        </div>
+        
+    );
+}
+
+
+export default findUsers;
---
+++ frontend/src/Pages/Users/UpdateUsers/updateUser.js
@@ -0,0 +1,16 @@
+import Container from "react-bootstrap/esm/Container";
+
+function updateUser(){
+    return(
+        <div>
+            <Container>
+                <h1>Update Users here</h1>
+            </Container>
+            
+        </div>
+        
+    );
+}
+
+
+export default updateUser;
