a022d0af480e36ced5d51ba72f2a7e4459ed4d5e
Gabino Aguirre <gaguirr5@cougarnet.uh.edu>
2023-04-19 20:48:10 -0500

    add addUser api

--- backend/env/api.py
+++ backend/env/api.py
@@ -57,12 +57,38 @@ cnx = create_connection(
     os.getenv('DATABASE')
 )
 
+def findOcurrences(string, char):
+    return [i for i, x in enumerate(string) if x == char]
+
+def addSingleQuote(var):
+    indexes = findOcurrences(var, "'")
+    print('62', var, indexes)
+    if len(indexes) > 0:
+        # for quote in indexes:
+        #     var = f"{var[:quote]}\{var[quote:]}"
+        ch = r"\'"
+        var = var.replace("'", ch)
+        print(var)
+    return var
+
 
 #get all users
 @app.route("/allUsers", methods=['GET'])
-def hello_world():
+def getUsers():
     sql = """SELECT * FROM users; """
     users = execute_read_query(cnx, sql)
     print(users)
     return jsonify(users)
 
+@app.route("/addUser", methods=['Post'])
+def addUser():
+    request_data = request.get_json()
+    print(request_data)
+    firstName = addSingleQuote(request_data['FirstName'])
+    middleName = addSingleQuote(request_data['MiddleName'])
+    lastName = addSingleQuote(request_data['LastName'])
+    Choice1, Choice2, Choice3, Choice4, Choice5, Choice6, Choice7, Choice8, Choice9, Choice10 = addSingleQuote(request_data['Choice1']), addSingleQuote(request_data['Choice2']), addSingleQuote(request_data['Choice3']), addSingleQuote(request_data['Choice4']), addSingleQuote(request_data['Choice5']), addSingleQuote(request_data['Choice6']), addSingleQuote(request_data['Choice7']), addSingleQuote(request_data['Choice8']), addSingleQuote(request_data['Choice9']), addSingleQuote(request_data['Choice10'])
+    sql = f"""INSERT INTO Users(`FirstName`, `MiddleName`, `LastName`, `Restaurants`) VALUES("{firstName}", "{middleName}", "{lastName}", JSON_ARRAY("{Choice1}", "{Choice2}", "{Choice3}", "{Choice4}", "{Choice5}", "{Choice6}", "{Choice7}", "{Choice8}", "{Choice9}", "{Choice10}"))"""
+    print('94', sql)
+    execute_query(cnx, sql)
+    return 'New User Added'
